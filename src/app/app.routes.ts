77b19a01923afa0eb747b4aca512388f4d95dfa2
Aarin Deb <aarindeb2003@gmail.com>
2026-07-13 01:49:19 +0200

    init project et stucture

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home';
+import { AboutComponent } from './pages/about/about';
+import { ProductComponent } from './pages/product/product';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'product/:id', component: ProductComponent }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -5,7 +5,7 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: '../styles.css',
 })
 export class App {
   protected readonly title = signal('au-petit-village');
