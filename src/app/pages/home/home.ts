9406c1a020e98a4c012b7a0562a3ac44545626e9
Aarin Deb <aarindeb2003@gmail.com>
2026-07-13 18:35:49 +0200

    Homepage template complet + ajouts des fonts

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router'; // Indispensable pour le lien vers le produit
 import { ProductService } from '../../services/product.service';
 import { Product } from '../../models/product.model';
 import { FilterByNamePipe } from '../../pipes/filter-by-name.pipe';
@@ -9,14 +10,15 @@ import { SortbyPricePipe } from '../../pipes/sortby-price.pipe';
 @Component({
   selector: 'app-home',
   standalone: true,
-  // Déclaration des mdules ou pipes utilisés dans ce composant
-  imports: [CommonModule, FormsModule, FilterByNamePipe, SortbyPricePipe],
+  imports: [CommonModule, FormsModule, RouterLink, FilterByNamePipe, SortbyPricePipe],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class HomeComponent implements OnInit {
-  searchTerm: string = ''; // Cette ligne corrige l'erreur sur searchTerm
-  products: Product[] = []; // Cette ligne corrige l'erreur sur products
+  searchTerm: string = '';
+  // On ajoute la variable de tri (croissant par défaut)
+  sortDirection: 'asc' | 'desc' = 'asc'; 
+  products: Product[] = [];
 
   constructor(private productService: ProductService) {}
 
