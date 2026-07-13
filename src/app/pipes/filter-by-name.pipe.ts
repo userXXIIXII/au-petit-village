a1f37b7da96ad011ae045d15c6bd81fbe684dc39
Aarin Deb <aarindeb2003@gmail.com>
2026-07-13 02:08:43 +0200

    Création pipe filtrer par recherche

--- src/app/pipes/filter-by-name.pipe.ts
+++ src/app/pipes/filter-by-name.pipe.ts
@@ -1,1 +1,18 @@
 // Pour filtrer la liste des figurines selon ce que l'utilisateur tape dans une barre de recherche
+
+import { Pipe, PipeTransform } from '@angular/core';
+import { Product } from '../models/product.model';
+
+@Pipe({
+    name: 'filterByName',
+    standalone: true
+})
+export class FilterByNamePipe implements PipeTransform {
+    transform(products: Product[], searchTerm: string): Product[] {
+        if (!products || !searchTerm) return products;
+        
+        return products.filter(product => 
+            product.name.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+    }
+}
