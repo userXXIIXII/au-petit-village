954484d16a6f10cbd5f5b207fdd4263d3c56907b
Aarin Deb <aarindeb2003@gmail.com>
2026-07-13 19:49:39 +0200

    Formulaire de contact complet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home';
 import { AboutComponent } from './pages/about/about';
 import { ProductComponent } from './pages/product/product';
+import { ContactComponent } from './pages/contact/contact';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'about', component: AboutComponent },
-    { path: 'product/:id', component: ProductComponent }
+    { path: 'product/:id', component: ProductComponent },
+    { path: 'contact', component: ContactComponent }
 ];
---
+++ src/app/pages/contact/contact.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact.html',
+  styleUrl: './contact.css',
+})
+export class ContactComponent {}
